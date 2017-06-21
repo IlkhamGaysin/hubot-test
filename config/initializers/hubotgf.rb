b384fd4528e9d5b82f239f0bce7366462eb47a40
IlkhamGaysin <ilgamgaysin@gmail.com>
2017-06-21 12:55:11 +0300

    Correct configuration for hubotgf

--- config/initializers/hubotgf.rb
+++ config/initializers/hubotgf.rb
@@ -1,4 +1,3 @@
 HubotGf.configure do |config|
-  config.hubot_url = ENV.fetch('HUBOT_URL') # default interface/port for Hubot
-  config.catchall_worker = nil
+  config.hubot_url = ENV.fetch('HUBOT_URL')
 end
